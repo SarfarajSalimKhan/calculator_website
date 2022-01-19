5f9695e6a262dfbfba00f074a00423fc184df484
SarfarajSalimKhan <sarfaraz.salimkhan17@rediffmail.com>
2022-01-19 02:50:47 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function insert(number) {
+document.form.result.value = document.form.result.value + number;
+}
+
+function equal() {
+var expression = document.form.result.value;
+if(expression) {
+document.form.result.value = eval(expression);
+}
+}
+
+function backspace() {
+var expression = document.form.result.value;
+document.form.result.value = expression.substring(0, expression.length - 1);
+}
+
+function clear() {
+document.form.result.value = '';
+}
