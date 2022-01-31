c6f8ef42bbebf3ed61e4cab6b049834e0050a7e1
sarfaraz.salimkhan17@rediffmail.com <SarfarajSalimKhan>
2022-01-31 10:56:13 +0000

    updated index.html

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function insert(number) {
-document.form.result.value = document.form.result.value + number;
+    document.form.result.value = document.form.result.value + number;
 }
 
 function equal() {
-var expression = document.form.result.value;
-if(expression) {
-document.form.result.value = eval(expression);
-}
+    var expression = document.form.result.value;
+    if (expression) {
+        document.form.result.value = eval(expression);
+    }
 }
 
 function backspace() {
-var expression = document.form.result.value;
-document.form.result.value = expression.substring(0, expression.length - 1);
+    var expression = document.form.result.value;
+    document.form.result.value = expression.substring(0, expression.length - 1);
 }
 
 function clean() {
-document.form.result.value = '';
+    document.form.result.value = '';
 }
